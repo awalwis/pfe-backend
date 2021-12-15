650afe11e6c35a2150472eefbb0b5d44136a5535
NathanFichefet <71337468+NathanFichefet@users.noreply.github.com>
2021-12-15 14:19:26 +0100

    add route ads for user

--- app.py
+++ app.py
@@ -192,6 +192,23 @@ def get_all_ads():
         return jsonify({e.__class__.__name__: e.args[0]}), 500
 
 
+@ app.route('/api/annonces/user/<int:id_user>', methods=['GET'])
+def get_all_ads_idUser(id_user):
+    try:
+        decodedToken = jwt.decode(request.headers.get(
+            'Authorization'), "sdkfh5464sdfjlskdjfntmdjfhskjfdhs", algorithms=["HS256"])
+        if(decodedToken['role'] != "admin"):
+            if(id_user != decodedToken['id_user']):
+                raise ValueError("NOT AUTHORIZED")
+
+        result = database.getAdWithIdUser(id_user)
+        return jsonify({'ads': result}), 200
+    except (jwt.InvalidTokenError) as e:
+        return jsonify({e.__class__.__name__: "INVALID TOKEN"}), 500
+    except (Exception) as e:
+        return jsonify({e.__class__.__name__: e.args[0]}), 500
+
+
 @ app.route('/api/annonces/<int:ad_id>', methods=['GET'])
 def get_ad(ad_id):
     try:
